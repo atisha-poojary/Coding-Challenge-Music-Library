707b20311d8f5454ddde642b46418f08fd16431c
atisha-poojary <atishyam@gmail.com>
2017-01-20 17:56:26 -0500

    Initial Commit

---
+++ Coding Challenge: Music Search/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coding Challenge: Music Search
+//
+//  Created by Atisha Poojary on 20/01/17.
+//  Copyright © 2017 Atisha Poojary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
